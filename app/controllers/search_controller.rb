67bd209a948b1c4155a308888c4080efff30f29b
DESKTOP-GPOD44V\허유경 <veronica_33@naver.com>
2018-10-13 15:51:57 +0900

    search modify

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,5 +3,11 @@ class SearchController < ApplicationController
   end
 
   def list
+    @posts = Post.all
+    if params[:query]
+      @posts = Post.search(params[:query]).order("created_at DESC")
+    else
+      @posts = Post.all.order('created_at DESC')
+    end
   end
 end
