e4d8b91fec0ec884e6d03333850db72df70735b9
cho <wjcho0511@naver.com>
2018-10-14 18:16:35 +0900

    .

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :update, :edit, :destroy]
 
   # view 있는 것
   def index
