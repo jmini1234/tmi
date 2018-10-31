79b7c349585a3a3b67e48907e902f2f657939722
soomti <soom93@gmail.com>
2018-10-31 11:32:14 +0900

    api route devise add

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :posts
+      devise_scope :user do
+        post "/sign_in", :to => 'sessions#create'
+        post "/sign_up", :to => 'registrations#create'
+        delete "/sign_out", :to => 'sessions#destroy'
+      end
     end
   end
 
