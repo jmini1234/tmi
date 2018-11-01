a660f4b49e477acc666a7f13f10037e26b26c84d
jmini1234 <shjj09@naver.com>
2018-11-01 20:37:20 +0900

    last_modify

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,4 +1,6 @@
 class MypageController < ApplicationController
+  before_action :authenticate_user!
+
   def my_post
     @posts = Post.where("user_id = ?", current_user.id)
   end
@@ -7,20 +9,18 @@ class MypageController < ApplicationController
     @bookmarks = Bookmark.where("user_id = ?", current_user.id)
   end
 
-  def bookmark_create
-    @bookmark = Bookmark.new
-    @bookmark.post_id = params[:id]
-    @bookmark.user_id = current_user.id
-    @bookmark.save
-
-    redirect_to :back
-  end
-
-  def bookmark_destroy
-    @bookmark = Bookmark.where(post_id: :id, user_id: :user_id)
-    @bookmark.destroy
-    redirect_to '/'
-  end
+  # def bookmark_create
+  #   @bookmark = Bookmark.new
+  #   @bookmark.post_id = params[:id]
+  #   @bookmark.user_id = current_user.id
+  #   @bookmark.save
+  #
+  #   redirect_to :back
+  # end
+  #
+  # def bookmark_destroy
+  #   @bookmark.destroy
+  # end
 
   def new_news
 
@@ -33,4 +33,16 @@ class MypageController < ApplicationController
   def test
 
   end
+
+  def bookmark_toggle
+    like = Bookmark.find_by(user_id: current_user.id, post_id: params[:post_id])
+     if like.nil?
+        Bookmark.create(user_id: current_user.id, post_id: params[:post_id])
+       else
+         like.destroy
+       end
+       redirect_to :back
+    end
+
+
 end
